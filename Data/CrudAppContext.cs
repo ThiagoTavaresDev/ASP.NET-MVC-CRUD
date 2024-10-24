c3928513bef543d864932b3ce8143521e3d71672
Thiago Tavares <Thiagoff2019@hotmail.com>
2024-10-24 10:42:03 -0300

    crud asp.net core mvc

---
+++ Data/CrudAppContext.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CrudApp.Models;
+
+namespace CrudApp.Data
+{
+    public class CrudAppContext : DbContext
+    {
+        public CrudAppContext (DbContextOptions<CrudAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CrudApp.Models.Movie> Movie { get; set; } = default!;
+    }
+}
